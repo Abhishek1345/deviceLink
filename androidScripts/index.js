444db3b5bd9992d73694927dbbad28f7184ba913
Abhishek <jawanpuriaabhishek@gmail.com>
2026-09-10 21:58:49 +0530

    intital node scripts for android device

---
+++ androidScripts/index.js
@@ -0,0 +1,25 @@
+const { io } = require("socket.io-client");
+const path=require('path');
+const fs=require('fs');
+const socket = io("http://192.168.1.103:6969");
+socket.on('connect', () => {
+  console.log('Connected to server over local network!');
+  socket.emit('device_message', { hello: 'from second device' });
+});
+
+socket.on('ack', (response) => {
+  console.log('Server acknowledged:', response);
+});
+socket.on('file',({fileName,fileData})=>{
+  console.log(fileName);
+  const writePath=path.join('/storage/emulated/0/Download',fileName);
+  try{
+    fs.writeFileSync(writePath,fileData);
+  }
+  catch(err){
+    console.log(err);
+  }
+  finally{
+    console.log('saved file');
+  }
+})
