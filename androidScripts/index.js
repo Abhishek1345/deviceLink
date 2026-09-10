30366cfe8bef378a1e86ec8f1f045467c5666294
Abhishek <jawanpuriaabhishek@gmail.com>
2026-09-10 22:25:53 +0530

    connecting using device name over mDNS instead of using IPv4

--- androidScripts/index.js
+++ androidScripts/index.js
@@ -1,7 +1,7 @@
 const { io } = require("socket.io-client");
 const path=require('path');
 const fs=require('fs');
-const socket = io("http://192.168.1.103:6969");
+const socket = io("http://LAPTOP-8VCLDCU2.local:6969");
 socket.on('connect', () => {
   console.log('Connected to server over local network!');
   socket.emit('device_message', { hello: 'from second device' });
